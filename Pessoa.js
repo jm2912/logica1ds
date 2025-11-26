d45229a568f812184cb2d360ff76933328dd0b03
jm2912 <joao.faria.santos29@escola.pr.gov.br>
2025-11-26 14:50:24 -0300

    Create Pessoa.js

---
+++ Pessoa.js
@@ -0,0 +1,27 @@
+class Pessoa {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  apresentar() {
+    console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+  }
+}
+class Pessoa {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  apresentar() {
+    console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+  }
+}
+Elabore e acrescente no código original um trecho com exemplo de teste que: 
+
+- Crie uma instância da classe Pessoa com valores fictícios. 
+
+- Chame o método apresentar() dessa instância.
+
+- Mostre o resultado esperado no console.
